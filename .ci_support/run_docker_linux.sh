f9b1bd20915eea16fbf49191d9fbba1fdb13ff43
Julien Schueller <schueller@phimeca.com>
2025-11-28 18:13:10 +0100

    debug

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 set -xe
+export SOURCE_DATE_EPOCH=1640995200
+
 
 cd /tmp
-mkdir build && cd build
+git clone --depth 1 -b repro https://github.com/jschueller/sphinx.git
+pip install ./sphinx --break-system-packages
+cd /tmp
+
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -D_GLIBCXX_ASSERTIONS" \
       -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-shadow" \
-      -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
+      -DUSE_SPHINX=ON -DSPHINX_FLAGS="-T -j4" \
       /io
 make install
-make tests
-ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
+#make tests
+#ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
 
-uid=$1
-gid=$2
-if test -n "${uid}" -a -n "${gid}"
+UID_GID=$1
+if test -n "${UID_GID}"
 then
   sudo cp -r ~/.local/share/doc/*/html /io
-  sudo chown -R ${uid}:${gid} /io/html
+  sudo cp -r /tmp/tmp* /io/html
+  sudo chown -R ${UID_GID} /io/html
 fi
